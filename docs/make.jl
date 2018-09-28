bed89e9833fc800b029511fa6d5bbcd9055cdc4c
mjb3 <martinjosephburke@gmail.com>
2018-09-28 15:49:47 +0100

    docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,12 @@
 using Discuit
 using Documenter
+# create docs
 makedocs(doctest = false)
+# deploy github
+deploydocs(
+    repo = "github.com/USER_NAME/PACKAGE_NAME.jl.git",
+)
+
+## pdf (didn't work)
 # using DocumenterLaTeX
 # makedocs(format = :latex)
