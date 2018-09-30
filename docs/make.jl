a7a0d28ab5fc74cd18727cd9241e3869dab8e83a
mjb3 <martinjosephburke@gmail.com>
2018-09-30 01:02:51 +0100

    mdx bib

--- docs/make.jl
+++ docs/make.jl
@@ -4,7 +4,7 @@ using Documenter
 # create docs
 makedocs(modules=[Discuit], doctest=true)
 # deploy to GitHub
-deploydocs(deps = Deps.pip("mkdocs", "python-markdown-math", "mkdocs-bootswatch") # "mkdocs-bootstrap386"
+deploydocs(deps = Deps.pip("mkdocs", "python-markdown-math", "mkdocs-bootswatch", "mdx_bib") # "mkdocs-bootstrap386"
     , repo = "github.com/mjb3/Discuit.jl.git"
     , julia  = "nightly"
     , osname = "linux")
