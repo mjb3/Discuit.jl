2831a71232f6677b7e6d8f95c2e40866331430a7
mjb3 <martinjosephburke@gmail.com>
2018-11-04 05:00:16 +0000

    doc restore

--- docs/make.jl
+++ docs/make.jl
@@ -1,9 +1,10 @@
 using Discuit
 using Documenter
 
-# create docs
+## create docs
 # modules=[Discuit],
+# makedocs(sitename="Discuit.jl documentation")
 makedocs(sitename="Discuit.jl documentation", doctest=true)
-# deploy to GitHub
+## deploy to GitHub
 deploydocs(deps = Deps.pip("mkdocs", "python-markdown-math", "mkdocs-bootswatch") #"mdx_bib", "mkdocs-bootstrap386"
     , repo = "github.com/mjb3/Discuit.jl.git")
