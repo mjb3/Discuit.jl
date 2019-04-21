48452b2c6c0ccf7f49b23defc188acc2cd8d41ac
mjb3 <martinjosephburke@gmail.com>
2019-04-21 17:20:28 +0100

    type agnostic std prop (for eval)

--- src/Discuit.jl
+++ src/Discuit.jl
@@ -340,7 +340,7 @@ end
 # end # end of std proposal function
 
 ## standard proposal function
-function standard_proposal(model::PrivateDiscuitModel, xi::MarkovState, xf_parameters::ParameterProposal)
+function standard_proposal_old(model::PrivateDiscuitModel, xi::MarkovState, xf_parameters::ParameterProposal)
     ## choose proposal type
     prop_type = rand(1:3)
     # trajectory proposal
@@ -386,6 +386,60 @@ function standard_proposal(model::PrivateDiscuitModel, xi::MarkovState, xf_param
             splice!(xf_trajectory.event_type, evt_i)
             # compute ln g(x)
             prop_lk = ec / (model.obs_data.time[end] - t0)
+        end # end of insert/delete
+    end
+    ## evaluate full likelihood for trajectory proposal and return
+    return MarkovState(xi.parameters, xf_trajectory, compute_full_log_like(model, xi.parameters.value, xf_trajectory), prop_lk, prop_type)
+end # end of std proposal function
+
+## standard proposal function
+function standard_proposal(model::PrivateDiscuitModel, xi::MarkovState, xf_parameters::ParameterProposal)
+    ## choose proposal type
+    prop_type = rand(1:3)
+    # trajectory proposal
+    # - NEED TO MAKE THIS MORE EFFICIENT ****
+    xf_trajectory = deepcopy(xi.trajectory)
+    t0 = (model.t0_index == 0) ? 0.0 : xf_parameters.value[model.t0_index]
+    if prop_type == 3
+        ## move
+        length(xi.trajectory.time) == 0 && (return MarkovState(xf_parameters, xi.trajectory, NULL_LOG_LIKE, DF_PROP_LIKE, prop_type))
+        # - IS THERE A MORE EFFICIENT WAY TO DO THIS? I.E. ROTATE using circshift or something?
+        # choose event and define new one
+        evt_i = rand(1:length(xi.trajectory.time))
+        evt_tm = (rand() * (model.obs_data.time[end] - t0)) + t0 #, xi.trajectory.event_type[evt_i])
+        evt_tp = xi.trajectory.event_type[evt_i]
+        # remove old one
+        splice!(xf_trajectory.time, evt_i)
+        splice!(xf_trajectory.event_type, evt_i)
+        # add new one
+        add_event!(xf_trajectory, evt_tp, evt_tm)
+        # compute ln g(x)
+        prop_lk = 1.0
+    else
+        ## insert / delete
+        # choose type and count
+        # tp = rand(1:size(model.m_transition, 1))
+        # ec = get_event_type_count(xf_trajectory, tp)
+        if prop_type == 1
+            tp = rand(1:size(model.m_transition, 1))
+            ## insert at randomly chosen time
+            add_event!(xf_trajectory, tp, (rand() * (model.obs_data.time[end] - t0)) + t0)
+            ## compute ln g(x)
+            prop_lk = (size(model.m_transition, 1) * (model.obs_data.time[end] - t0)) / length(xf_trajectory.time)
+        else
+            ## delete
+            # println(" deleting... tp:", tp, " - ec: ", ec)
+            length(xi.trajectory.time) == 0 && (return MarkovState(xi.parameters, xf_trajectory, NULL_LOG_LIKE, DF_PROP_LIKE, prop_type))
+            # choose event index (repeat if != tp)
+            evt_i = rand(1:length(xi.trajectory.time))
+            # while xi.trajectory.event_type[evt_i] != tp
+            #     evt_i = rand(1:length(xi.trajectory.time))
+            # end
+            # remove
+            splice!(xf_trajectory.time, evt_i)
+            splice!(xf_trajectory.event_type, evt_i)
+            # compute ln g(x)
+            prop_lk = length(xi.trajectory.time) / ((model.obs_data.time[end] - t0) * size(model.m_transition, 1))
         end # end of insert/delete
     end
     ## evaluate full likelihood for trajectory proposal and return
