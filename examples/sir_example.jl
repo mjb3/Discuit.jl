d9e909d59a272278e330f79761de94bc075dfa4d
mjb3 <martinjosephburke@gmail.com>
2019-04-22 11:17:37 +0100

    same

--- examples/sir_example.jl
+++ examples/sir_example.jl
@@ -18,14 +18,14 @@ function sir_example()
     obs = x.observations
     # Discuit.print_observations(obs, "./out/sir_obs.csv")
 
-    Random.seed!(1)
+    Random.seed!(4)
     ## MCMC (STD)
     # rs = Discuit.run_met_hastings_mcmc(model, obs, [0.001, 0.1], 300000, 50000, false)
     # Discuit.tabulate_mcmc_results(rs, true)
     # println(Discuit.plot_parameter_trace(rs, 1))
     # println(Discuit.plot_parameter_heatmap(rs, 1, 2))
     ## convergence
-    gmn = Discuit.run_gelman_diagnostic(model, obs, [0.001 0.1; 0.002 0.1; 0.003 0.1], 100000, 20000, false)
+    gmn = Discuit.run_gelman_diagnostic(model, obs, [0.001 0.1; 0.002 0.1; 0.003 0.1], 200000, 20000, false)
     Discuit.tabulate_gelman_results(gmn, true)
 
     ## MCMC (MBP)
@@ -33,7 +33,7 @@ function sir_example()
     # Discuit.tabulate_mcmc_results(rs, true)
     # println(Discuit.plot_parameter_trace(rs, 1))
     # println(Discuit.plot_parameter_heatmap(rs, 1, 2))
-    gmn = Discuit.run_gelman_diagnostic(model, obs, [0.001 0.1; 0.002 0.1; 0.003 0.1])
+    gmn = Discuit.run_gelman_diagnostic(model, obs, [0.001 0.1; 0.002 0.1; 0.003 0.1], 100000, 20000)
     Discuit.tabulate_gelman_results(gmn, true)
 
 end
